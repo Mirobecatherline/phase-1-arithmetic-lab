319e736bdcf175f080af0b2693ca74308a91e7b6
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-07 18:19:18 +0300

    commit tree 0dd2e8645a3141bc67d4923dae3a3437b9351f21

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1=31,num2=2
+let multiply=0;
+multiply=num1*num2;
+const random=Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+const num3=22;
+const num4=6
+let mod=num3 % num4;
+let max=Math.max(1,2,20);
